81dd9d597ac9699841ba54174c280654d8db90cd
Gian Hiltbrunner <gian.silvan.hiltbrunner@gmail.com>
2020-07-15 17:12:06 +0200

    Initial Commit

---
+++ Zurigrad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Zurigrad
+//
+//  Created by Gian Hiltbrunner on 15.07.20.
+//  Copyright © 2020 Gian Hiltbrunner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Zurigrad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zurigrad
+//
+//  Created by Gian Hiltbrunner on 15.07.20.
+//  Copyright © 2020 Gian Hiltbrunner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
